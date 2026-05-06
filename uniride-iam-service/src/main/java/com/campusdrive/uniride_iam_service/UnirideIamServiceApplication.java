361cf804859a7ee04e0041d157b1729f00fbc73b
Kevin <171992754+KevCast1604@users.noreply.github.com>
2026-05-06 12:08:08 -0500

    feat: add initial commit and configuration

---
+++ uniride-iam-service/src/main/java/com/campusdrive/uniride_iam_service/UnirideIamServiceApplication.java
@@ -0,0 +1,13 @@
+package com.campusdrive.uniride_iam_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnirideIamServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnirideIamServiceApplication.class, args);
+	}
+
+}
